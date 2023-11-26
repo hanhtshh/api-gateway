3533d76e9d57094a515e4fb40adf3f237107a3b6
nguyenhanh4 <nguyenhanh4@telio.vn>
2023-11-26 11:53:21 +0700

    change order router to oder

--- api/middleware/authOrderMiddleware.js
+++ api/middleware/authOrderMiddleware.js
@@ -3,7 +3,7 @@ const { ORDER_PROXY_URLS } = require("../routers/proxy");
 const { authUser, authenAdmin } = require("./authenticateMiddleware");
 
 const authOrderMiddleware = async (request, response, next) => {
-    const ORDERUrl = request.originalUrl.replace(`/order`, '');
+    const ORDERUrl = request.originalUrl.replace(`/oder`, '');
     const forwardable = Object.keys(ORDER_PROXY_URLS).find((url) => url.includes(request.method + ORDERUrl));
     if (!forwardable) {
         return response.status(400).json('bad request');;
